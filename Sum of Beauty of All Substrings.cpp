c225c27c8e7372fa7569da1312524fff7736cddd
virendra jangid <94829041+Vrajjangid5@users.noreply.github.com>
2024-09-23 20:43:42 +0530

    Create Sum of Beauty of All Substrings.cpp

---
+++ Sum of Beauty of All Substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans = 0;
+    int beautySum(string s) {
+
+        for (int i = 0; i < s.length(); i++) {
+            map<char, int> mp;
+            for (int j = i; j < s.length(); j++) {
+                // counting the frequency of each character
+                mp[s[j]]++;
+
+                int leastFrequent = INT_MAX;
+                int mostFrequent = INT_MIN;
+                // Finding most frequent and least frequent chracter
+                for (auto it : mp) {
+                    leastFrequent = min(leastFrequent, it.second);
+                    mostFrequent = max(mostFrequent, it.second);
+                }
+                ans += mostFrequent - leastFrequent;
+            }
+        }
+
+        return ans;
+    }
+};
